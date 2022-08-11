07027dcd46b50b510e5fc34d964ecc6d1352a3c7
Belay Adamu <belay.adamu@engineering.outlier.org>
2022-08-11 23:39:46 +0300

    feat: return different error responses for development and production environments

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+const sendErrorDev = (err, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  })
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    })
+  }
+
+  // Programming or other unknown error: don't leak error details
+  // eslint-disable-next-line no-console
+  console.error('ERROR: ', err)
+
+  res.status(err.statusCode).json({
+    status: 'error',
+    message: 'Something went very wrong!'
+  })
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
+  if (process.env.NODE_ENV === 'development') {
+    return sendErrorDev(err, res)
+  }
+
+  sendErrorProd(err, res)
 }
