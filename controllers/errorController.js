a02f060dc35e2d042d733a34be3eb54cfdee7a3a
Belay Adamu <belay.adamu@engineering.outlier.org>
2022-08-31 18:38:51 +0300

    feat: implement proper mongoose error handeling for prod environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('../utils/appError')
+
+const handleCastErrorDB = (err) =>
+  new AppError(`Invalid ${err.path}: ${err.value}`, 400)
+
+const handleDuplicateFieldsDB = (err) => {
+  console.log(err)
+  const duplicateField = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)[0]
+
+  return new AppError(
+    `Duplicate field in ${duplicateField}. Please use another value`,
+    400
+  )
+}
+
+const handleValidationErrorsDB = (err) => {
+  const errorMessages = Object.values(err.errors).map((value) => value.message)
+  return new AppError(`Invalid input Data: ${errorMessages.join('. ')}`, 400)
+}
+
 const sendErrorDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
@@ -33,5 +53,11 @@ module.exports = (err, req, res, next) => {
     return sendErrorDev(err, res)
   }
 
-  sendErrorProd(err, res)
+  let error = JSON.parse(JSON.stringify(err))
+
+  if (error.name === 'CastError') error = handleCastErrorDB(error)
+  if (error.code === 11000) error = handleDuplicateFieldsDB(err)
+  if (error.name === 'ValidationError') error = handleValidationErrorsDB(err)
+
+  sendErrorProd(error, res)
 }
