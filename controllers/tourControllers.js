6a2d4c4365e6375834279d2ab13f8eb196be681a
Belay Adamu <belay.adamu@engineering.outlier.org>
2022-08-31 14:57:01 +0300

    feat: send correct error responses from tour route handler functions

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel')
 const { ApiFeatures } = require('../utils/apiFeatures')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
 exports.getTop5BestTours = async (req, res, next) => {
   req.query.limit = '5'
@@ -33,6 +34,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id)
+
+  if (!tour) {
+    return next(new AppError(`No tour found with id ${req.params.id}`, 404))
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -57,6 +63,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
   })
 
+  if (!updatedTour) {
+    return next(new AppError(`No tour found with id ${req.params.id}`, 404))
+  }
+
   res.status(204).json({
     status: 'success',
     data: {
@@ -66,7 +76,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 })
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id)
+  const tour = await Tour.findByIdAndDelete(req.params.id)
+
+  if (!tour) {
+    return next(new AppError(`No tour found with id ${req.params.id}`, 404))
+  }
+
   res.status(204).json({
     status: 'success'
   })
