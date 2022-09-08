8b40ab3d65160ee17e7ad0fafb3fc485623ff97f
Belay Adamu <belay.adamu@engineering.outlier.org>
2022-09-08 14:51:04 +0300

    feat: issue a JWT-token upon signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 
 exports.signup = catchAsync(async (req, res) => {
   const newUser = await User.create(req.body)
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.TOKEN_EXPIRATION
+  })
+
   res.status(200).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
